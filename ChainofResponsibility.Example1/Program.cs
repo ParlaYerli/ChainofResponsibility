5e03cce1556e83006d4a74b6a6533bc5b95d7bc4
AYSE PARLA YERLI <ayse.yerli@lcwaikiki.com>
2024-10-20 19:48:42 +0300

    Add project files.

---
+++ ChainofResponsibility.Example1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace ChainofResponsibility.Example1
+{
+    // Abstract Handler
+    public abstract class ComplaintHandler
+    {
+        protected ComplaintHandler nextHandler;
+
+        public void SetNextHandler(ComplaintHandler nextHandler)
+        {
+            this.nextHandler = nextHandler;
+        }
+
+        public abstract void HandleRequest(string complaint);
+    }
+
+    // Concrete Handler 1: Support Team
+    public class SupportTeamHandler : ComplaintHandler
+    {
+        public override void HandleRequest(string complaint)
+        {
+            if (complaint == "Basit sorun")
+            {
+                Console.WriteLine("Support: Sorunu çözdüm.");
+            }
+            else if (nextHandler != null)
+            {
+                nextHandler.HandleRequest(complaint);
+            }
+        }
+    }
+
+    // Concrete Handler 2: Analysis Team
+    public class AnalysisTeamHandler : ComplaintHandler
+    {
+        public override void HandleRequest(string complaint)
+        {
+            if (complaint == "Orta düzey sorun")
+            {
+                Console.WriteLine("AnalysisTeam: Sorunu çözdüm.");
+            }
+            else if (nextHandler != null)
+            {
+                nextHandler.HandleRequest(complaint);
+            }
+        }
+    }
+
+    // Concrete Handler 3: Developer Team
+    public class DeveloperTeamHandler : ComplaintHandler
+    {
+        public override void HandleRequest(string complaint)
+        {
+            if (complaint == "Ciddi sorun")
+            {
+                Console.WriteLine("Developer: Sorunu çözdüm.");
+            }
+            else
+            {
+                Console.WriteLine("Developer: Sorunu çözemedik.");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var support = new SupportTeamHandler();
+            var analysis = new AnalysisTeamHandler();
+            var developer = new DeveloperTeamHandler();
+
+            support.SetNextHandler(analysis);
+            analysis.SetNextHandler(developer);
+
+            string complaint = "Orta düzey sorun";
+            support.HandleRequest(complaint);
+
+            //string seriousComplaint = "Ciddi sorun";
+            //support.HandleRequest(seriousComplaint);
+        }
+    }
+}
---
+++ ChainofResponsibility.Example2/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace ChainofResponsibility.Example2
+{
+    public abstract class OrderHandler
+    {
+        protected OrderHandler nextHandler;
+
+        public void SetNextHandler(OrderHandler nextHandler)
+        {
+            this.nextHandler = nextHandler;
+        }
+
+        public void Handle(Order order)
+        {
+            try
+            {
+                if (ProcessOrder(order))
+                {
+                    Console.WriteLine("İşlem başarılı, zincir devam ediyor.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("İşlem başarısız, zincir tamamlandı.");
+                Console.WriteLine($"Hata: {ex.Message}");
+                return;
+            }
+
+            if (nextHandler != null)
+            {
+                nextHandler.Handle(order);
+            }
+        }
+
+        protected abstract bool ProcessOrder(Order order);
+    }
+
+    // Concrete Handler 1: Order 
+    public class ValidationHandler : OrderHandler
+    {
+        protected override bool ProcessOrder(Order order)
+        {
+            if (order.IsValid)
+            {
+                Console.WriteLine("Sipariş geçerli.");
+                return true;
+            }
+            else
+            {
+                throw new Exception("Sipariş geçersiz.");
+            }
+        }
+    }
+
+    // Concrete Handler 2: Stock Check
+    public class StockHandler : OrderHandler
+    {
+        protected override bool ProcessOrder(Order order)
+        {
+            if (order.IsInStock)
+            {
+                Console.WriteLine("Stokta yeterli ürün var.");
+                return true;
+            }
+            else
+            {
+                throw new Exception("Stokta yeterli ürün yok.");
+            }
+        }
+    }
+
+    // Concrete Handler 3: Payment 
+    public class PaymentHandler : OrderHandler
+    {
+        protected override bool ProcessOrder(Order order)
+        {
+            if (order.IsPaymentCompleted)
+            {
+                Console.WriteLine("Ödeme başarıyla tamamlandı.");
+                return true; 
+            }
+            else
+            {
+                throw new Exception("Ödeme başarısız.");
+            }
+        }
+    }
+
+
+    // Concrete Handler 4: Invoice Generation
+    public class InvoiceHandler : OrderHandler
+    {
+        protected override bool ProcessOrder(Order order)
+        {
+            Console.WriteLine("Fatura oluşturuldu ve müşteriye gönderildi.");
+            return true; 
+        }
+    }
+
+    // Order class
+    public class Order
+    {
+        public bool IsValid { get; set; }
+        public bool IsPaymentCompleted { get; set; }
+        public bool IsInStock { set; get; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                IsValid = true,              // Sipariş geçerli 
+                IsInStock = false,            // Stokta ürün var
+                IsPaymentCompleted = true   // Ödeme başarısız
+            };
+
+            var validation = new ValidationHandler();
+            var payment = new PaymentHandler();
+            var stock = new StockHandler();
+            var invoice = new InvoiceHandler();
+
+            validation.SetNextHandler(stock);
+            stock.SetNextHandler(payment);
+            payment.SetNextHandler(invoice);
+
+            validation.Handle(order);
+        }
+    }
+
+}
